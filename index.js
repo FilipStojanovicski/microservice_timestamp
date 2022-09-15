1c017ccece571fd255cf0d9d14ee41d1c906efa1
Filip Stoyanovic <filipstoyanovic@Filips-MacBook-Pro-2.local>
2022-09-15 16:08:35 -0400

    completed project

--- index.js
+++ index.js
@@ -18,10 +18,29 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+const utc_date_re = /^\d{4}-\d{2}-\d{2}$/
+const unix_date_re = /^\d+$/
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:timestamp", function (req, res) {
+  result = {}
+  if (req.params.timestamp.match(utc_date_re)){
+    console.log("UTC");
+    var date = new Date(req.params.timestamp);
+    result.unix = Math.floor(date.getTime());
+    result.utc = date.toUTCString();
+    res.json(result);
+  }
+  else if (req.params.timestamp.match(unix_date_re)){
+    console.log("Unix");
+    result.unix = parseInt(req.params.timestamp);
+    var date = new Date(parseInt(req.params.timestamp));
+    result.utc = date.toUTCString();
+    res.json(result)
+  }
+  else{
+    res.json({error: "Invalid Date"})
+  }
+
 });
 
 
